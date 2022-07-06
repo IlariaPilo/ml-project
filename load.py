6da6e88f7309a166a5739f6db7b7ad0986c4f832
ILARIA PILO s296847 <s296847@studenti.polito.it>
2022-07-06 09:29:08 +0200

    Add load utilities, as well as txt and binary files

---
+++ load.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+"""
+Loads the content of a file in a pair X (dataset) and L (labels), then saves them as npy files (to be used only once per file).
+:param file_name is the name of the file (WITHOUT EXTENSION)
+"""
+def convert_txt_to_np(file_name):
+    # open the file in read mode
+    fp = open(file_name+'.txt', 'r')
+    X = []
+    L = []
+    # read it line by line
+    while True:
+        line = fp.readline()
+        if not line:
+            break
+        # split the line
+        clean = [float(x.strip()) for x in line.split(',')]
+        X.append(clean[0:-1])
+        L.append(clean[-1])
+    fp.close()
+    X, L = np.array(X).T, np.array(L, dtype=int)
+    np.save(file_name+'X.npy', X)
+    np.save(file_name+'L.npy', L)
+
+
+"""
+Load the content of the pair file_name+'X.npy' and file_name+'L.npy'.
+:param file_name is the name of the file (WITHOUT EXTENSION)
+"""
+def load(file_name):
+    return np.load(file_name+'X.npy'), np.load(file_name+'L.npy')
--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import numpy as np
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import load
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # 1. read the training dataset
+    trainX, trainL = load.load("data/Train")
+    # 2. feature analysis
+    a = 1
